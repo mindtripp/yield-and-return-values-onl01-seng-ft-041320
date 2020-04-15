327b2626936207b8d77d1aa0eb25f2ce895ee00e
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-15 20:02:47 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,13 @@
+require 'pry'
 def hello(array)
   i = 0
+ binding.pry
   while i < array.length
-   binding.pry
+  
     yield(array[i])
     i += 1
   end
 end
-
 
 binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
